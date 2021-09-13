1993f22fd282dc5d38d959931a356910f1a56812
kojibeloved <llippe20@student.aau.dk>
2021-09-13 12:35:02 +0200

    Assignment 15 laurits
    Assignment 15 laurits

--- src/session0/G_Exercises/Problem15/Team.java
+++ src/session0/G_Exercises/Problem15/Team.java
@@ -1,24 +1,25 @@
-package session0.G_Exercises.Problem15;
-
 public class Team {
-    String name;
-    Player p1, p2;
+	String teamName;
+	Player player1;
+	Player player2;
 
-    Team(String name, Player one, Player two) {
-        this.name = name;
-        this.p1 = one;
-        this.p2 = two;
-    }
+	Team(String team, Player p1, Player p2){
+		this.teamName = team;
+		this.player1 = p1;
+		this.player2 = p2;
+	}
 
-    double averageAge() {
-        return (p1.age + p2.age) / 2;
-    }
+	double teamAverage(){
+		return (((double) player1.age + (double) player2.age) / 2);
+	}
 
-    void printInfo() {
-        String teamInfo = String.format("Teamname: %s, Average Age: %.2f", this.name, averageAge());
-        System.out.println(teamInfo);
-        System.out.println("\tPlayer1) " + p1.toString());
-        System.out.println("\tPlayer2) " + p2.toString());
-        System.out.println();
-    }
+
+	//
+	double playerWithHighestRatioOnTheTeam(){
+		if (this.player1.ratio() > this.player2.ratio()){
+			return this.player1.ratio();
+		}
+		else
+			return this.player2.ratio();
+	}
 }
