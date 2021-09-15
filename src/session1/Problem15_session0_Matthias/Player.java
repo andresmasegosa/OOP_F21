25f3110a7632333c3aafcf995af2916aaec954a8
andresmasegosa <andresmasegosa@gmail.com>
2021-09-15 11:33:58 +0200

    Mathias's solution to Problem 15.

---
+++ src/session1/Problem15_session0_Matthias/Player.java
@@ -0,0 +1,38 @@
+package session1.Problem15_session0_Matthias;
+
+public class Player {
+    String firstName, lastName;
+    int age,
+        wins = 0,
+        losses = 0;
+    String sponsor = "";
+
+    Player(String fn, String ln, int age) {
+        this.firstName = fn;
+        this.lastName = ln;
+        this.age = age;
+    }
+    Player(String fn, String ln, int age, String sponsor) {
+        this.firstName = fn;
+        this.lastName = ln;
+        this.age = age;
+        this.sponsor = sponsor;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder(
+                String.format(
+                        "Name: %s %s, Age: %d, Wins: %d, Losses: %d, W/L Ratio: %.2f",
+                        this.firstName, this.lastName, this.age, this.wins, this.losses, this.ratio()
+                )
+        );
+        if(!this.sponsor.isEmpty())
+            sb.append(" (Sponsor: ").append(this.sponsor).append(')');
+
+        return sb.toString();
+    }
+
+    double ratio() {
+        return (this.losses != 0 && this.wins != 0) ? (double) this.wins / (double) this.losses : 0.0;
+    }
+}
---
+++ src/session1/exercise3/PersonTest.java
@@ -0,0 +1,10 @@
+package session1.exercise3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+
+}
