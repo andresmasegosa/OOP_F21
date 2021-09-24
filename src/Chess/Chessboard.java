236ba327791c06d8f50b9ad4806d3b11c4107dbf
tgrenaae <tgrena20@student.aau.dk>
2021-09-24 22:15:36 +0200

    deleted Location.java and Pawn.java to make better OOP.

--- src/Chess/Chessboard.java
+++ src/Chess/Chessboard.java
@@ -1,49 +1,139 @@
 package Chess;
 
+import java.util.Scanner;
+
 public class Chessboard {
-    Location[][] piecesLocation;
+    Piece[][] piecesOnChessboard;
 
     public Chessboard() {
-        this.piecesLocation = new Location[8][8];
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                this.piecesLocation[i][j] = new Location(i,j);
-            }
-        }
+        this.piecesOnChessboard = new Piece[8][8];
     }
 
     public void printChessBoard(){
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++){
-                System.out.println(this.piecesLocation[i][j]);
+                System.out.print(this.piecesOnChessboard[i][j].getPieceSymbol());
             }
+            System.out.println();
+        }
+    }
+
+    public void setupNewChessGame(){
+        //prepare array to handle pieces by filling with empty pieces (squares with no piece on it)
+        Piece noPiece = new Piece(Color.NO_COLOR, PieceNames.NO_PIECE);
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                this.piecesOnChessboard[i][j] = noPiece;
+            }
+        }
+        //create white pieces
+        Piece whitePawn = new Piece(Color.WHITE,PieceNames.PAWN);
+        Piece whiteRook = new Piece(Color.WHITE, PieceNames.ROOK);
+        Piece whiteHorse = new Piece(Color.WHITE, PieceNames.HORSE);
+        Piece whiteBishop = new Piece(Color.WHITE, PieceNames.BISHOP);
+        Piece whiteQueen = new Piece(Color.WHITE, PieceNames.QUEEN);
+        Piece whiteKing = new Piece(Color.WHITE, PieceNames.KING);
+
+        for(int i = 0; i < 8; i++){
+            this.piecesOnChessboard[1][i] = whitePawn;
+        }
+        this.piecesOnChessboard[0][0] = whiteRook;
+        this.piecesOnChessboard[0][7] = whiteRook;
+        this.piecesOnChessboard[0][1] = whiteHorse;
+        this.piecesOnChessboard[0][6] = whiteHorse;
+        this.piecesOnChessboard[0][2] = whiteBishop;
+        this.piecesOnChessboard[0][5] = whiteBishop;
+        this.piecesOnChessboard[0][3] = whiteQueen;
+        this.piecesOnChessboard[0][4] = whiteKing;
+
+
+        //create black pieces
+        Piece blackPawn = new Piece(Color.BLACK,PieceNames.PAWN);
+        Piece blackRook = new Piece(Color.BLACK, PieceNames.ROOK);
+        Piece blackHorse = new Piece(Color.BLACK, PieceNames.HORSE);
+        Piece blackBishop = new Piece(Color.BLACK, PieceNames.BISHOP);
+        Piece blackQueen = new Piece(Color.BLACK, PieceNames.QUEEN);
+        Piece blackKing = new Piece(Color.BLACK, PieceNames.KING);
+
+        for(int i = 0; i < 8; i++){
+            this.piecesOnChessboard[6][i] = blackPawn;
+        }
+        this.piecesOnChessboard[7][0] = blackRook;
+        this.piecesOnChessboard[7][7] = blackRook;
+        this.piecesOnChessboard[7][1] = blackHorse;
+        this.piecesOnChessboard[7][6] = blackHorse;
+        this.piecesOnChessboard[7][2] = blackBishop;
+        this.piecesOnChessboard[7][5] = blackBishop;
+        this.piecesOnChessboard[7][3] = blackQueen;
+        this.piecesOnChessboard[7][4] = blackKing;
+    }
+
+    public boolean checkLegalMove(Piece piece, int xCurrent, int yCurrent, int xFuture, int yFuture){
+        switch (piece.nameOfPiece){
+            case PAWN -> {
+                return piece.pawnMove(xCurrent, yCurrent, xFuture, yFuture);
+            }
+            case ROOK -> {
+                return piece.rookMove(xCurrent,yCurrent,xFuture,yFuture);
+            }
+            case HORSE -> {
+                return false;
+            }
+            case BISHOP -> {
+                return false;
+            }
+            case QUEEN -> {
+                return false;
+            }
+            case KING -> {
+                return false;
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
+
+    public void playChessGame(){
+        //create Scanner object to take user input
+        Scanner moveInput = new Scanner(System.in);
+        boolean stillPlaying = true;
+        while (stillPlaying){
+            System.out.print("Enter move (9 9 9 9 to exit): ");
+            int xCurrent = moveInput.nextInt() - 1;
+            int yCurrent = moveInput.nextInt() - 1;
+            int xFuture = moveInput.nextInt() - 1;
+            int yFuture = moveInput.nextInt() - 1;
+
+            if(xCurrent > 7 && yCurrent > 7 && xFuture > 7 && yFuture > 7){
+                stillPlaying = false;
+                System.out.println("Game over.");
+                return;
+            }
+            //System.out.println(this.piecesOnChessboard[yCurrent][xCurrent].nameOfPiece);
+
+            boolean isLegalMove = this.checkLegalMove(this.piecesOnChessboard[yCurrent][xCurrent], xCurrent, yCurrent, xFuture, yFuture);
+            //System.out.println("the move is legal: " + isLegalMove);
+            if(isLegalMove){
+                this.piecesOnChessboard[yFuture][xFuture] = this.piecesOnChessboard[yCurrent][xCurrent];
+                this.piecesOnChessboard[yCurrent][xCurrent] = new Piece(Color.NO_COLOR, PieceNames.NO_PIECE);
+            }
+            this.printChessBoard();
         }
     }
 
     public static void main(String[] args) {
         //create new chessboard
         Chessboard ChessBoard = new Chessboard();
-        //ChessBoard.printChessBoard();
 
-        //create white pieces
-        Piece whitePawn = new Piece(Color.WHITE, PieceNames.PAWN);
-        Piece whiteRook = new Piece(Color.WHITE, PieceNames.ROOK);
-        Piece whiteKnight = new Piece(Color.WHITE, PieceNames.KNIGHT);
-        Piece whiteBishop = new Piece(Color.WHITE, PieceNames.BISHOP);
-        Piece whiteQueen = new Piece(Color.WHITE, PieceNames.QUEEN);
-        Piece whiteKing = new Piece(Color.WHITE, PieceNames.KING);
+        //setup pieces for a new game
+        ChessBoard.setupNewChessGame();
 
-        //create white pawns
-        Pawn whiteAPawn = new Pawn(whitePawn, ChessBoard.piecesLocation[0][1]);
-        Pawn whiteBPawn = new Pawn(whitePawn, ChessBoard.piecesLocation[1][1]);
-        Pawn whiteCPawn = new Pawn(whitePawn, ChessBoard.piecesLocation[2][1]);
-        Pawn whiteDPawn = new Pawn(whitePawn, ChessBoard.piecesLocation[3][1]);
-        Pawn whiteEPawn = new Pawn(whitePawn, ChessBoard.piecesLocation[4][1]);
-        Pawn whiteFPawn = new Pawn(whitePawn, ChessBoard.piecesLocation[5][1]);
-        Pawn whiteGPawn = new Pawn(whitePawn, ChessBoard.piecesLocation[6][1]);
-        Pawn whiteHPawn = new Pawn(whitePawn, ChessBoard.piecesLocation[7][1]);
+        //print the chess board to start the game
+        ChessBoard.printChessBoard();
 
+        //start playing chess
+        ChessBoard.playChessGame();
 
-        System.out.println(whiteAPawn);
     }
 }
--- src/Chess/Piece.java
+++ src/Chess/Piece.java
@@ -2,16 +2,18 @@ package Chess;
 
 enum Color {
     WHITE,
-    BLACK
+    BLACK,
+    NO_COLOR
 }
 
 enum PieceNames{
     PAWN,
     ROOK,
-    KNIGHT,
+    HORSE,
     BISHOP,
     QUEEN,
-    KING
+    KING,
+    NO_PIECE
 }
 public class Piece {
     Color playerColor;
@@ -20,13 +22,78 @@ public class Piece {
     public Piece(Color playerColor, PieceNames nameOfPiece) {
         this.playerColor = playerColor;
         this.nameOfPiece = nameOfPiece;
+
+    }
+
+    public String getPieceSymbol(){
+        switch (this.nameOfPiece) {
+            case PAWN -> {
+               return "P ";
+            }
+            case ROOK -> {
+                return "R ";
+            }
+            case HORSE -> {
+                return "H ";
+            }
+            case BISHOP -> {
+                return "B ";
+            }
+            case QUEEN -> {
+                return "Q ";
+            }
+            case KING -> {
+                return "K ";
+            }
+            default -> {
+                return "  ";
+            }
+        }
+    }
+
+    boolean pawnMove(int xCurrent, int yCurrent, int xFuture, int yFuture){
+        //check if white pawn is at start location
+        if(this.playerColor == Color.WHITE){
+            if(yCurrent == 1){
+                if((xCurrent == xFuture) && (yFuture - yCurrent) == 2){
+                    return true;
+                }
+            }
+            //checks if white pawn move is 1 square forward
+            else if((xCurrent == xFuture) && (yFuture - yCurrent == 1)){
+                return true;
+            }
+
+        }
+
+        //else runs only if piece is black
+        else{
+            //check if black pawn is at start location
+            if(yCurrent == 6){
+                if((xCurrent == xFuture) && (yCurrent - yFuture) == 2){
+                    return true;
+                }
+            }
+            //checks if black pawn move is 1 square forward
+            else if((xCurrent == xFuture) && (yCurrent - yFuture == 1)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean rookMove(int xCurrent, int yCurrent, int xFuture, int yFuture){
+        if((xCurrent == xFuture) ||(yCurrent == yFuture)){
+            return true;
+        }
+        return false;
     }
 
     @Override
     public String toString() {
         return "Piece{" +
-                "playerColor='" + playerColor + '\'' +
-                ", nameOfPiece='" + nameOfPiece + '\'' +
+                "playerColor=" + playerColor +
+                ", nameOfPiece=" + nameOfPiece +
                 '}';
     }
 }
