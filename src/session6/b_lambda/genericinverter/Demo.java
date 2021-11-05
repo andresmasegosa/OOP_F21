cb3fc1a65463977c7bcf032db749861c5260ca08
Andres R. Masegosa <andresmasegosa@gmail.com>
2021-11-05 12:09:40 +0100

    Code for Session 6

---
+++ src/session6/b_lambda/genericinverter/Demo.java
@@ -0,0 +1,23 @@
+package session6.b_lambda.genericinverter;
+
+import session6.b_lambda.inverter.DivisionInverter;
+import session6.b_lambda.inverter.Inverter;
+
+import java.util.function.Function;
+
+public  class Demo {
+
+    public static double invertAndAddOne(Function<Double,Double> f, double x) {
+        return f.apply(x + 1.0);
+    }
+
+    public static void main(String[] args) {
+        double result = invertAndAddOne(x -> 1./x, 42.0);
+        System.out.println(result);
+
+        // Exercise: Implements the inverter f(x) = 1/(x*x+10) using lambda expressions.
+
+    }
+
+}
+
